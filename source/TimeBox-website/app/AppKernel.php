60a05f78bce089cfb76a0a77a8616dac15b341f9
TheLightBird <thelightbird@gmail.com>
2014-02-09 23:36:50 +0100

    Added FOSUserBundle

--- source/TimeBox-website/app/AppKernel.php
+++ source/TimeBox-website/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new TimeBox\MainBundle\TimeBoxMainBundle(),
             new TimeBox\UserBundle\TimeBoxUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ source/TimeBox-website/src/TimeBox/UserBundle/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace TimeBox\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="TimeBox\UserBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
